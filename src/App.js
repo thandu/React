d53b45ff503ae4d2bb8fb0e53a41d8e13c355aef
Nagaraju <tnr.mtech@gmail.com>
2020-04-25 11:24:49 +0530

    Single Page Applications Part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import Main from './components/MainComponent';
-import "./App.css";
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
 
 class App extends Component {
-  
+
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
-  
 }
 
 export default App;
